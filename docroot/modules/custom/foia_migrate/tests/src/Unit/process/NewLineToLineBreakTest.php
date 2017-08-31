62e20adfed4c99176b047dc9561a303a68f63115
Dan Gurin <dan@dangur.com>
2017-08-31 10:10:23 -0400

    use array build test as model

--- docroot/modules/custom/foia_migrate/tests/src/Unit/process/NewLineToLineBreakTest.php
+++ docroot/modules/custom/foia_migrate/tests/src/Unit/process/NewLineToLineBreakTest.php
@@ -16,40 +16,28 @@ use Drupal\Tests\migrate\Unit\process\MigrateProcessTestCase;
  * @group foia_migrate
  */
 class NewLineToLineBreakTest extends MigrateProcessTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    $configuration = [
+      'is_xhtml' => TRUE,
+    ];
+    $this->plugin = new NewlineToLineBreak($configuration, 'nl2br', []);
+    parent::setUp();
+  }
+
   /**
    * Tests transformation.
    *
-   * @dataProvider lineEndingsDataProvider
-   *
-   * @param $configuration
-   *   The configuration of the migration process plugin.
-   * @param $value
-   *   The source value for the migration process plugin.
-   * @param $expected
-   *   The expected value of the migration process plugin.
    */
-  public function testNewLineToLineBreak($configuration, $value, $expected) {
-    $this->plugin = new NewlineToLineBreak($configuration, 'nl2br', []);
-    $actual = $this->plugin->transform($value, $this->migrateExecutable, $this->row, 'body/value');
+  public function testTransform() {
+    $source = '\n';
+    $expected = '<br>';
+    $value = $this->plugin->transform($source, $this->migrateExecutable, $this->row, 'body');
 
-    $this->assertEquals($expected, $actual);
+    $this->assertEquals($value, $expected);
   }
 
-  /**
-   * Data provider of line endings.
-   *
-   * @return array
-   *  Array of line endings actual/expected values.
-   */
-  public function lineEndingsDataProvider() {
-    return [
-      'new_line' => [
-        'configuration' => [
-          'is_xhtml' => TRUE,
-        ],
-        'value' => '\n',
-        'expected' => '<br />',
-      ],
-    ];
-  }
 }
