7d8cc113169ab7f6e1b90dcda0069da1a199924e
Dan Gurin <dan@dangur.com>
2017-09-15 12:06:49 -0400

    DOJ-91: Adds unit test class.

---
+++ docroot/modules/custom/foia_api/tests/src/Unit/CreateRequestsFromSumbissionsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\Tests\foia_api\Unit;
+
+/**
+ * Class CreateRequestsFromSumbissionsTest.
+ *
+ * Tests webform submission generation via API.
+ *
+ * @package Drupal\Tests\foia_api\Unit
+ *
+ * @group api
+ */
+class CreateRequestsFromSumbissionsTest extends \PHPUnit_Framework_TestCase {
+
+}
