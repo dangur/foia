8feea50c40cb3fe88f3d44462d4538350134c320
Dan Gurin <dan@dangur.com>
2017-09-25 17:45:41 -0400

    DOJ-91: Adds functional test.

---
+++ docroot/modules/custom/foia_api/tests/src/Functional/WebformSubmissionResourceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\Tests\foia_api\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Class WebformSubmissionResourceTest.
+ *
+ * @package Drupal\Tests\foia_api\Functional
+ */
+class WebformSubmissionResourceTest extends BrowserTestBase {
+
+  /**
+   * Tests webform submission resource.
+   */
+  public function testWebformSubmissionResource() {
+
+  }
+
+}
