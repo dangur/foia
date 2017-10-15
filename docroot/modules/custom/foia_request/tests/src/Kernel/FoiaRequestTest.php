a48faf8f9c2bde09e18d383826905ebaa544ce20
vagrant <vagrant@local.dojfoia.gov>
2017-10-15 07:51:37 +0000

    DOJ-146: Work in progress.

--- docroot/modules/custom/foia_request/tests/src/Kernel/FoiaRequestTest.php
+++ docroot/modules/custom/foia_request/tests/src/Kernel/FoiaRequestTest.php
@@ -20,6 +20,7 @@ class FoiaRequestTest extends EntityKernelTestBase {
   public function setUp() {
     parent::setUp();
 
+    $this->installConfig('foia_request');
     $this->installEntitySchema('foia_request');
 
   }
@@ -28,6 +29,7 @@ class FoiaRequestTest extends EntityKernelTestBase {
    * Tests FOIA Request creation.
    */
   public function testFoiaRequest() {
+
     $foiaRequest = FoiaRequest::create([
       'field_agency_component' => 1,
       'field_case_management_id' => '1',
@@ -35,10 +37,26 @@ class FoiaRequestTest extends EntityKernelTestBase {
       'field_error_message' => 'error message',
       'field_http_code' => 200,
       'field_requester_email' => 'requester@example.com',
-      'field_submission_id' => 1,
       'field_submission_method' => 'api',
+      'field_submission_time' => 1508432427,
       'field_tracking_number' => '1',
+      'field_webform_submission_id' => 1,
     ]);
+print_r($foiaRequest->getSubmissionMethod());
+
+
+    $this->assertEquals('foia_request', $foiaRequest->getEntityTypeId());
+
+    $foiaEntity = \Drupal::entityTypeManager()
+      ->getStorage('foia_request')
+      ->loadByProperties(['field_case_management_id' => '1']);
+    print_r($foiaEntity);
+
+    /*$query = \Drupal::entityQuery('foia_request')->execute();
+    print_r($query);*/
+
+    //print_r($foiaRequest->get('field_agency_component')->getValue());
+    //print_r($time);
 
   }
 
